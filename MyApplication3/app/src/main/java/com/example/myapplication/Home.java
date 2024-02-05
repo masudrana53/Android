78a2933db80da0ea3caed3bb05a75f5373ba22e6
Masud Rana <masudrana.20091@gmail.com>
2024-02-05 13:16:43 +0600

    add

--- MyApplication3/app/src/main/java/com/example/myapplication/Home.java
+++ MyApplication3/app/src/main/java/com/example/myapplication/Home.java
@@ -2,15 +2,34 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class
 
 Home extends AppCompatActivity {
 
+    private Button btnContact;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        btnContact = findViewById(R.id.btnContact);
+
+        btnContact.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(getApplicationContext(),Contact.class);
+                startActivity(intent);
+
+            }
+        });
     }
 }
